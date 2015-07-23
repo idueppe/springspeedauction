4692e58215c1a19789556d68981ab2aab211fea1
idueppe <ingo.dueppe@dueppe.com>
2015-07-23 09:57:11 +0200

    Add DataSourceConfig

---
+++ src/main/java/de/crowdcode/speedauction/config/DataSourceConfig.java
@@ -0,0 +1,10 @@
+package de.crowdcode.speedauction.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+@Configuration("DataSourceConfig")
+public class DataSourceConfig {
+}
