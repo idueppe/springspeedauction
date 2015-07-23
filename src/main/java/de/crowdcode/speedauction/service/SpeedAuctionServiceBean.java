a380461b926f73dc9b6b2ad4fe5c2dbc9fb81d13
idueppe <ingo.dueppe@dueppe.com>
2015-07-23 08:59:50 +0200

    BeanAware interfaces implementiert.

--- src/main/java/de/crowdcode/speedauction/service/SpeedAuctionServiceBean.java
+++ src/main/java/de/crowdcode/speedauction/service/SpeedAuctionServiceBean.java
@@ -3,6 +3,8 @@ package de.crowdcode.speedauction.service;
 import de.crowdcode.speedauction.domain.Auction;
 import de.crowdcode.speedauction.domain.DetailProduct;
 import de.crowdcode.speedauction.repository.AuctionRepository;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,7 @@ import java.util.List;
  * @author Ingo Düppe (Crowdcode)
  */
 @Service("speedAuctionService")
-public class SpeedAuctionServiceBean implements SpeedAuctionService {
+public class SpeedAuctionServiceBean implements SpeedAuctionService, BeanNameAware, DisposableBean {
 
     @Autowired
     private AuctionRepository auctionRepository;
@@ -35,4 +37,13 @@ public class SpeedAuctionServiceBean implements SpeedAuctionService {
         return auctionRepository.findAll();
     }
 
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("------------------- Mein Name ist "+name);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("\n------------------- Bean wird entfernt!");
+    }
 }
